4f23494311903e28807504e5649af2ab1e3f6db8
pelaz <pelaz@MaxiiPC>
2023-04-19 00:42:13 -0300

    Modificado resultadoFecha, ahora muestra los dos mensajes del ejercicio 2 más claro. Creación de clase procesoString, su controlador y el archivo HTML.

---
+++ src/main/java/ar/edu/unju/edm/model/ProcesoString.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.edm.model;
+
+public class ProcesoString {
+	private String cadena;
+	private String mensaje1;
+	private String cadenaInvertida;
+	private String mensaje2;
+	public ProcesoString() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
